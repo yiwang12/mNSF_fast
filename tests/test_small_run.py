60aa77f73c0f18b442f63995e7583cbc10fd086c
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2024-10-31 20:19:18 +0100

    Update test_small_run.py

--- tests/test_small_run.py
+++ tests/test_small_run.py
@@ -55,6 +55,7 @@ def _run(
     )
 
     factors = inpf12["factors"][:, :n_loadings]
+    print(factors)
     for k in range(n_sample):
         indices = process_multiSample.get_listSampleID(D)[k].astype(int)
         pd.DataFrame(factors[indices, :]).to_csv(output_dir / f"factors_sample{k + 1:02d}_smallData.csv")
